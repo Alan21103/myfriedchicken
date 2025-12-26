d1c05ed76ee3dcceed9efe762393ae434d00616b
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2025-12-26 20:26:36 +0700

    dependecy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import path from "path" // Jika ini merah, jalankan: npm install -D @types/node
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
